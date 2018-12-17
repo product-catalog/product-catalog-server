0e6f0ea3dff30a6aea79a39777ffb44d2e2dd68a
siqqQ <b.bohosyan@gmail.com>
2018-12-17 20:46:04 +0200

    Spring Initlzr project created

---
+++ server/src/main/java/com/pcatalog/pcatalog/PcatalogApplication.java
@@ -0,0 +1,14 @@
+package com.pcatalog.pcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcatalogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcatalogApplication.class, args);
+    }
+
+}
+
