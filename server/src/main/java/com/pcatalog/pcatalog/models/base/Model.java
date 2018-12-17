29d6dbeb9555f9fbe009930506543c3cdf402e3b
siqqQ <b.bohosyan@gmail.com>
2018-12-17 22:28:21 +0200

    Added Model.java in models

---
+++ server/src/main/java/com/pcatalog/pcatalog/models/base/Model.java
@@ -0,0 +1,46 @@
+package com.pcatalog.pcatalog.models.base;
+
+
+@MappedSuperclass
+public abstract class Model {
+
+    @Id
+    @GeneratedValue
+    private Long recordId;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date recordCreated;
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date recordLastTimeEdited;
+
+    public Model() {
+        setRecordCreated(new Date());
+        setRecordLastTimeEdited(new Date());
+    }
+
+    public Long getRecordId() {
+        return recordId;
+    }
+
+    public void setRecordId(Long recordId) {
+        this.recordId = recordId;
+    }
+
+    public Date getRecordCreated() {
+        return recordCreated;
+    }
+
+    public void setRecordCreated(Date recordCreated) {
+        this.recordCreated = recordCreated;
+    }
+
+    public Date getRecordLastTimeEdited() {
+        return recordLastTimeEdited;
+    }
+
+    public void setRecordLastTimeEdited(Date recordLastTimeEdited) {
+        this.recordLastTimeEdited = recordLastTimeEdited;
+    }
+}
