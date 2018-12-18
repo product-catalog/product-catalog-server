3d93ef6d43056128eaf49a601978c572316680eb
siqqQ <b.bohosyan@gmail.com>
2018-12-18 13:49:28 +0200

    Added TestController

--- server/src/main/java/com/pcatalog/pcatalog/TestController.java
+++ server/src/main/java/com/pcatalog/pcatalog/TestController.java
@@ -1,16 +1,16 @@
 package com.pcatalog.pcatalog;
 
+import com.pcatalog.pcatalog.models.Photo;
 import com.pcatalog.pcatalog.models.Product;
 import com.pcatalog.pcatalog.repositories.PhotoRepository;
 import com.pcatalog.pcatalog.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/apply")
 public class TestController {
     @Autowired
     private PhotoRepository photoRepository;
@@ -18,8 +18,16 @@ public class TestController {
     private ProductRepository productRepository;
 
     @PostMapping("/new")
-    public ResponseEntity<Product> newProduct(@RequestBody Product product){
-        productRepository.save(product);
-        return new ResponseEntity<>(product, HttpStatus.OK);
+    public ResponseEntity<Product> newProduct(@RequestBody ProductDto product){
+        Photo photo = new Photo(product.getPhoto().getName(), product.getPhoto().getPhoto());
+        photoRepository.save(photo);
+        Product product1 = new Product(product.getName(), product.getDescription(), photo, product.getPrice());
+        productRepository.save(product1);
+        return new ResponseEntity<>(product1, HttpStatus.OK);
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "dfsfdfsdfdsfds";
     }
 }
