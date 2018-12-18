7e281836ebf8d51c0cdc89428f659a0fe4acd741
siqqQ <b.bohosyan@gmail.com>
2018-12-18 12:08:48 +0200

    added @Repository annotation to photo repository

--- server/src/main/java/com/pcatalog/pcatalog/repositories/PhotoRepository.java
+++ server/src/main/java/com/pcatalog/pcatalog/repositories/PhotoRepository.java
@@ -2,6 +2,8 @@ package com.pcatalog.pcatalog.repositories;
 
 import com.pcatalog.pcatalog.models.Photo;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhotoRepository extends CrudRepository<Photo, Long> {
 }
