0c396d22e015c404f129ff26ae0550ea623b4bd1
siqqQ <b.bohosyan@gmail.com>
2018-12-18 11:05:03 +0200

    Added model's vars, constr, getters and setters

--- server/src/main/java/com/pcatalog/pcatalog/models/Product.java
+++ server/src/main/java/com/pcatalog/pcatalog/models/Product.java
@@ -2,8 +2,51 @@ package com.pcatalog.pcatalog.models;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "products")
 public class Product {
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+    @NotEmpty
+    private String photo;
+    @NotEmpty
+    private Double price;
+
+    protected Product(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
